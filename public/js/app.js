5808f74b2dbe46b0ccb803395137dca14b1a8ba6
Your Name <you@example.com>
2021-01-14 17:14:03 -0700

    completed create component

--- public/js/app.js
+++ public/js/app.js
@@ -16,12 +16,12 @@ class Create extends React.Component {
                 <input type="text" id="date" onChange={this.props.handleChange} />
                 <br/>
 
-                <label htmlFor="name">Description</label>
+                <label htmlFor="description">Description</label>
                 <br/>
                 <input type="text" id="description" onChange={this.props.handleChange} />
                 <br/>
 
-                <label htmlFor="name">Image</label>
+                <label htmlFor="image">Image</label>
                 <br/>
                 <input type="text" id="image" onChange={this.props.handleChange} />
                 <br/>
@@ -34,72 +34,72 @@ class Create extends React.Component {
 }
 
 // Show Component
-class Show extends React.Component {
-    render = () => {
-        return <div id="show-trip-container">
-                <ul>
-                    {this.state.trips.map((trip) => {
-                        return <li key={trip._id}>
-                            <img src={trip.image} />
-                            <br/>
-
-                            {trip.name}
-                            <br/>
-
-                            {trip.date}
-                            <br/>
-
-                            {trip.description}
-                            <br/>
-
-                            <button value={trip._id} onClick={this.deleteTrip}>
-                                Remove
-                            </button>
-                            <br/>
-
-
-                        </li>
-                    }}
-                </ul>
-        </div>
-    }
-}
-
-// Edit Component
-class Edit extends React.Component {
-    render = () => {
-        return <div id="edit-trip-container">
-                            <details>
-                                <summary>Edit Trip Details</summary>
-                                <form id={trip._id} onSubmit={this.updateTrip}>
-
-                                    <label htmlFor="name">Name</label>
-                                    <br/>
-                                    <input type="text" id="name" defaultValue={trip.name} onChange={this.handleChange}/>
-                                    <br/>
-
-                                    <label htmlFor="date">Date</label>
-                                    <br/>
-                                    <input type="text" id="date" defaultValue={trip.date} onChange={this.handleChange}/>
-                                    <br/>
-
-                                    <label htmlFor="description">Description</label>
-                                    <br/>
-                                    <input type="text" id="description" defaultValue={trip.description} onChange={this.handleChange}/>
-                                    <br/>
-
-                                    <label htmlFor="name">Image</label>
-                                    <br/>
-                                    <input type="text" id="image" defaultValue={trip.image} onChange={this.handleChange}/>
-                                    <br/>
-
-                                    <input type="submit" value="Update Details" />
-
-                                </form>
-                            </details>
-                    </div>
-    }
-}
+// class Show extends React.Component {
+//     render = () => {
+//         return <div id="show-trip-container">
+//                 <ul>
+//                     {this.state.trips.map((trip) => {
+//                         return <li key={trip._id}>
+//                             <img src={trip.image} />
+//                             <br/>
+//
+//                             {trip.name}
+//                             <br/>
+//
+//                             {trip.date}
+//                             <br/>
+//
+//                             {trip.description}
+//                             <br/>
+//
+//                             <button value={trip._id} onClick={this.deleteTrip}>
+//                                 Remove
+//                             </button>
+//                             <br/>
+//
+//
+//                         </li>
+//                     }}
+//                 </ul>
+//         </div>
+//     }
+// }
+//
+// // Edit Component
+// class Edit extends React.Component {
+//     render = () => {
+//         return <div id="edit-trip-container">
+//                             <details>
+//                                 <summary>Edit Trip Details</summary>
+//                                 <form id={trip._id} onSubmit={this.updateTrip}>
+//
+//                                     <label htmlFor="name">Name</label>
+//                                     <br/>
+//                                     <input type="text" id="name" defaultValue={trip.name} onChange={this.handleChange}/>
+//                                     <br/>
+//
+//                                     <label htmlFor="date">Date</label>
+//                                     <br/>
+//                                     <input type="text" id="date" defaultValue={trip.date} onChange={this.handleChange}/>
+//                                     <br/>
+//
+//                                     <label htmlFor="description">Description</label>
+//                                     <br/>
+//                                     <input type="text" id="description" defaultValue={trip.description} onChange={this.handleChange}/>
+//                                     <br/>
+//
+//                                     <label htmlFor="name">Image</label>
+//                                     <br/>
+//                                     <input type="text" id="image" defaultValue={trip.image} onChange={this.handleChange}/>
+//                                     <br/>
+//
+//                                     <input type="submit" value="Update Details" />
+//
+//                                 </form>
+//                             </details>
+//                     </div>
+//     }
+// }
 
 
 // Parent Component
@@ -128,16 +128,18 @@ class App extends React.Component {
                 description: '',
                 image: ''
             })
+            document.getElementById('name').value = ""
+            document.getElementById('date').value = ""
+            document.getElementById('description').value = ""
+            document.getElementById('image').value = ""
+
         })
     }
-
-
-
     render = () => {
         return <div>
             <h1>Trips on Trips</h1>
 
-            <Create handleSubmit={this.handleSubmit} handleChange={this.handleChange} state={this.state}></Create>      
+            <Create handleSubmit={this.handleSubmit} handleChange={this.handleChange} state={this.state}></Create>
 
         </div>
     }
